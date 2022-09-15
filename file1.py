ab82afcb2c1e9fd12b8abc1c9a2bca3bed002861
AbadDeveloper <56825989+abaddeveloper@users.noreply.github.com>
2022-09-15 14:18:08 -0400

    new file added

---
+++ file1.py
@@ -0,0 +1,48 @@
+Last login: Thu Sep 15 11:55:46 on ttys000
+maryabad@Marys-Mac-mini-10 FirstGITProject % vi file1.y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("I'm file1")
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --
