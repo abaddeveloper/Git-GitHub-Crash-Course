63ea5479b0ea2e53a42ea80eeb9765610f247263
AbadDeveloper <56825989+abaddeveloper@users.noreply.github.com>
2022-10-03 18:50:42 -0400

    Add .gitignore file with IGNORE RULES

---
+++ file2.py
@@ -0,0 +1,48 @@
+Last login: Thu Sep 15 12:58:13 on ttys000
+maryabad@Marys-Mac-mini-11 FirstGITProject % vi file2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("I'm file2")
+
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --
